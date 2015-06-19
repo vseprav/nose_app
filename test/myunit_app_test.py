a9764a225e47176d5624de36dff20fdfb9c9deec
Ivan Lesnik <ilesnik@solanolabs.com>
2015-06-19 15:05:55 +0000

    Make it parallel

--- test/myunit_app_test.py
+++ test/myunit_app_test.py
@@ -3,6 +3,10 @@ import myunit.app
 
 class MyUnitTest(unittest.TestCase):
 
+  @staticmethod
+  def setUp():
+    print 4+"3"
+
   def testInit(self):
     app = myunit.app.App(100)
     self.assertEquals(app.var, 100)
