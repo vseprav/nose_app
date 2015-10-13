86e867d2da9c480766310795ba941126e88b37c2
Andrii Furmanets <furmanets.andriy@gmail.com>
2015-10-13 17:01:28 +0300

    Update myunit_app_test.py

--- test/myunit_app_test.py
+++ test/myunit_app_test.py
@@ -5,7 +5,7 @@ class MyUnitTest(unittest.TestCase):
 
   def testInit(self):
     app = myunit.app.App(100)
-    self.assertEquals(app.var, 100)
+    self.assertEquals(app.var, 101)
 
   def testGetVar(self):
     app = myunit.app.App(200)
