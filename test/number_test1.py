535ee551ae2a2d53659d71138312569b0388cb18
Andrii Peliak <vseprav@gmail.com>
2017-01-24 14:46:37 +0200

    add test whith emply result

--- test/number_test1.py
+++ test/number_test1.py
@@ -8,22 +8,6 @@ def one_test():
     print('5 seconds')
     for x in range(0, 10):
         print "We're on time %d" % (x)
-    time.sleep(5)
-    print('10 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('15 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('20 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('25 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
 
 def two_test():
     print('start test')
@@ -33,22 +17,6 @@ def two_test():
     print('5 seconds')
     for x in range(0, 10):
         print "We're on time %d" % (x)
-    time.sleep(5)
-    print('10 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('15 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('20 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('25 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
 
 def three_test():
     print('start test')
@@ -56,21 +24,5 @@ def three_test():
         print "We're on time %d" % (x)
     time.sleep(5)
     print('5 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('10 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('15 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('20 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
-    time.sleep(5)
-    print('25 seconds')
     for x in range(0, 10):
         print "We're on time %d" % (x)
