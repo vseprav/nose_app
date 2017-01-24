7b84fe8733505b4f64832aab9934be102b98300a
Andrii Peliak <vseprav@gmail.com>
2017-01-24 14:11:47 +0200

    add test whith emply result

--- test/number_test1.py
+++ test/number_test1.py
@@ -1,40 +1,77 @@
 import time
+import range
 
 def one_test():
     print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('5 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('10 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('15 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('20 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('25 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
 
 def two_test():
     print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('5 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('10 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('15 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('20 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('25 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
 
 def three_test():
     print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('5 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('10 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('15 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('20 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
     time.sleep(5)
     print('25 seconds')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
