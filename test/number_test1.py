f5a080f84a73e60cb6cfb6983fba2644823a5c65
Andrii Peliak <vseprav@gmail.com>
2017-01-24 15:25:44 +0200

    add test whith emply result

--- test/number_test1.py
+++ test/number_test1.py
@@ -1,35 +1,65 @@
-import time
 
-def one_test():
+def test_1():
     print('start test')
     for x in range(0, 10):
         print "We're on time %d" % (x)
-    time.sleep(5)
-    print('5 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
 
-    assert 1
-
-def two_test():
+def test_2():
     print('start test')
     for x in range(0, 10):
         print "We're on time %d" % (x)
-    time.sleep(5)
-    print('5 seconds')
-    for x in range(0, 10):
-        print "We're on time %d" % (x)
 
-    assert 1
-
-
-def three_test():
+def test_3():
     print('start test')
     for x in range(0, 10):
         print "We're on time %d" % (x)
-    time.sleep(5)
-    print('5 seconds')
+
+def test_4():
+    print('start test')
     for x in range(0, 10):
         print "We're on time %d" % (x)
 
-    assert 1
+def test_5():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_6():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_7():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_8():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_9():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_10():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_11():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_12():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
+
+def test_13():
+    print('start test')
+    for x in range(0, 10):
+        print "We're on time %d" % (x)
