62a6ea4f03744a8be425b25793d4d32fafcd36f2
Andrii Peliak <vseprav@gmail.com>
2017-01-25 17:24:21 +0200

    add test whith emply result

--- test/number_test2.py
+++ test/number_test2.py
@@ -2,9 +2,8 @@ import time
 import sys
 
 def test_1():
-    time.sleep(100)
     print('start test_1')
-    for x in range(0, 20):
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
-
+    time.sleep(100)
     sys.stdout.flush()
--- test/number_test3.py
+++ test/number_test3.py
@@ -2,9 +2,9 @@ import time
 import sys
 
 def test_1():
-    time.sleep(100)
+    time.sleep(10)
     print('start test_1')
-    for x in range(0, 20):
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
