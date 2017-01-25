25876b3607e4e3f666cf3c0beeedfce9cdd7040b
Andrii Peliak <vseprav@gmail.com>
2017-01-25 15:11:08 +0200

    add test whith emply result

--- test/number_test1.py
+++ test/number_test1.py
@@ -2,32 +2,40 @@ import time
 import sys
 
 def test_1():
+    time.sleep(10)
     print('start test_1')
-    for x in range(0, 2000000):
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
-    time.sleep(10)
 
-    for x in range(0, 2000000):
+def test_2():
+    time.sleep(10)
+    print('start test_2')
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
-    time.sleep(10)
 
-    for x in range(0, 2000000):
+def test_3():
+    time.sleep(10)
+    print('start test_3')
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
+def test_4():
     time.sleep(10)
-
-    for x in range(0, 2000000):
+    print('start test_4')
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
-    time.sleep(10)
 
-    for x in range(0, 2000000):
+def test_5():
+    time.sleep(10)
+    print('start test_5')
+    for x in range(0, 2000):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
