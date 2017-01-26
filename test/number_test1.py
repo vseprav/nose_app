750c9b12305142855588c50e47601f989fc1ab69
Andrii Peliak <vseprav@gmail.com>
2017-01-26 16:26:38 +0200

    add test whith emply result

--- test/number_test1.py
+++ test/number_test1.py
@@ -4,7 +4,7 @@ import sys
 def test_1():
     time.sleep(100)
     print('start test_1')
-    for x in range(0, 20):
+    for x in range(0, 50):
         print("We're on time %d" % (x))
 
     sys.stdout.flush()
