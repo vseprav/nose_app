c1d9e912ccc8b40e5eb8c46860ffd619f1b26568
Andrii Peliak <vseprav@gmail.com>
2017-02-03 12:42:48 +0200

    add empty test

--- test/number_test1.py
+++ test/number_test1.py
@@ -1,7 +1,3 @@
-import sys
-
 def test_1():
     for x in range(0, 10):
         print "We're on time %d" % (x)
-
-    sys.exit(0)
--- test/number_test2.py
+++ test/number_test2.py
@@ -1,4 +1,2 @@
-import sys
-
 def test_1():
-    sys.exit(0)
+    pass
