c2723e90bc8843b792cc2a8fecd7a8c85e8a6380
Andrii Peliak <vseprav@gmail.com>
2017-02-03 10:40:06 +0200

    add empty test

--- test/number_test1.py
+++ test/number_test1.py
@@ -1,10 +1,2 @@
-import time
-import sys
-
 def test_1():
-    time.sleep(100)
-    print('start test_1')
-    for x in range(0, 50):
-        print("We're on time %d" % (x))
-
-    sys.stdout.flush()
+    pass
--- test/number_test2.py
+++ test/number_test2.py
@@ -1,9 +1,2 @@
-import time
-import sys
-
 def test_1():
-    print('start test_1')
-    for x in range(0, 12):
-        print("We're on time %d" % (x))
-    time.sleep(100)
-    sys.stdout.flush()
+    pass
